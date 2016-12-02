df673774af0134197b16c1e60f8a3e4694d5cbc8
veselints <veselintsvetanov@gmail.com>
2016-12-02 17:14:10 +0200

    Add Mails grid and details view

--- TelerikMvcWebMail/Controllers/HomeController.cs
+++ TelerikMvcWebMail/Controllers/HomeController.cs
@@ -1,37 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using TelerikMvcWebMail.Models;
 
 namespace TelerikMvcWebMail.Controllers
 {
-	public class HomeController : Controller
-	{
-		public ActionResult Index()
-		{
-			ViewBag.Message = "Welcome to ASP.NET MVC!";
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var mailsData = new List<MailViewModel>();
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Thread.Sleep(1000);
+                }
 
-			return View();
-		}
+                var mail = new MailViewModel()
+                {
+                    CheckBoxCheked = false,
+                    From = "Veselin Tsvetanov",
+                    Subject = i + "Check this mail!",
+                    Date = DateTime.Now,
+                    Text = "This is a simple mail with a simple content"
+                };
 
-		public ActionResult NewMail()
-		{
-			return View();
-		}
+                mailsData.Add(mail);
+            }
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your app description page.";
+            return View(mailsData);
+        }
 
-			return View();
-		}
+        public ActionResult NewMail()
+        {
+            return View();
+        }
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
 
-			return View();
-		}
-	}
+            return View();
+        }
+    }
 }
---
+++ TelerikMvcWebMail/Models/MailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelerikMvcWebMail.Models
+{
+    public class MailViewModel
+    {
+        public bool CheckBoxCheked { get; set; }
+
+        public string From { get; set; }
+
+        public string Subject { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Text { get; set; }
+    }
+}
