a8d2dcf4529de2f30abf991297ded2c881b988cb
veselints <veselintsvetanov@gmail.com>
2016-12-08 15:33:32 +0200

    Add Contacts cards view

--- TelerikMvcWebMail/Controllers/ContactsController.cs
+++ TelerikMvcWebMail/Controllers/ContactsController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TelerikMvcWebMail.Models;
 
 namespace TelerikMvcWebMail.Controllers
 {
@@ -19,5 +22,66 @@ namespace TelerikMvcWebMail.Controllers
 		{
 			return View();
 		}
-	}
+
+        public ActionResult Contacts_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var contacts = new List<ContactViewModel>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                var currentContact = new ContactViewModel()
+                {
+                    EmployeeID = i,
+                    Address = "Some random adress, " + i,
+                    City = "Sofia",
+                    Country = "Bulgaria",
+                    FirstName = "Pesho",
+                    LastName = "Goshev",
+                    HomePhone = "+356 676 616 71"
+                };
+
+                contacts.Add(currentContact);
+            }
+
+            return Json(contacts.ToDataSourceResult(request));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Contacts_Create([DataSourceRequest] DataSourceRequest request, ContactViewModel product)
+        {
+            var results = new List<ContactViewModel>();
+
+            //if (product != null && ModelState.IsValid)
+            //{
+            //    productService.Create(product);
+            //    results.Add(product);
+            //}
+
+            //return Json(results.ToDataSourceResult(request, ModelState));
+
+            return Json(results);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Contacts_Update([DataSourceRequest] DataSourceRequest request, ContactViewModel product)
+        {
+            if (product != null && ModelState.IsValid)
+            {
+                //productService.Update(product);
+            }
+
+            return Json(ModelState.ToDataSourceResult());
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Contacts_Destroy([DataSourceRequest] DataSourceRequest request, ContactViewModel product)
+        {
+            if (product != null)
+            {
+                //productService.Destroy(product);
+            }
+
+            return Json(ModelState.ToDataSourceResult());
+        }
+    }
 }
---
+++ TelerikMvcWebMail/Models/ContactViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TelerikMvcWebMail.Models
+{
+    public class ContactViewModel
+    {
+        public int EmployeeID
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string EmployeeName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Country
+        {
+            get;
+            set;
+        }
+
+        public string City
+        {
+            get;
+            set;
+        }
+
+        public string Address
+        {
+            get;
+            set;
+        }
+
+        public string HomePhone
+        {
+            get;
+            set;
+        }
+    }
+}
