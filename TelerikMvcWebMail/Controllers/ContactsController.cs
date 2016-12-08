2bdd567c65a76e29b66909c8d8d109e7f9e262d8
veselints <veselintsvetanov@gmail.com>
2016-12-08 16:30:16 +0200

    Add list Contacts view

--- TelerikMvcWebMail/Controllers/ContactsController.cs
+++ TelerikMvcWebMail/Controllers/ContactsController.cs
@@ -27,7 +27,7 @@ namespace TelerikMvcWebMail.Controllers
         {
             var contacts = new List<ContactViewModel>();
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10; i++)
             {
                 var currentContact = new ContactViewModel()
                 {
