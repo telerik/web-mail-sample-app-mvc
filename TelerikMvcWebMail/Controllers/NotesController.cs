ccf4a2044c2a7475fa51926312d1a4683aab2c4c
veselints <veselintsvetanov@gmail.com>
2016-12-08 17:13:56 +0200

    Add Notes grid and search functionality

--- TelerikMvcWebMail/Controllers/NotesController.cs
+++ TelerikMvcWebMail/Controllers/NotesController.cs
@@ -1,22 +1,55 @@
-﻿using System;
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using TelerikMvcWebMail.Models;
 
 namespace TelerikMvcWebMail.Controllers
 {
-	public class NotesController : Controller
-	{
-		// GET: /Notes/
-		public ActionResult Notes()
-		{
-			return View();
-		}
+    public class NotesController : Controller
+    {
+        // GET: /Notes/
+        public ActionResult Notes()
+        {
+            return View();
+        }
 
-		public ActionResult NewNote()
-		{
-			return View();
-		}
-	}
+        public ActionResult NewNote()
+        {
+            return View();
+        }
+
+        public ActionResult Notes_Read([DataSourceRequest] DataSourceRequest request, string search)
+        {
+            var notesData = new List<NoteViewModel>();
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Thread.Sleep(1000);
+                }
+
+                var note = new NoteViewModel()
+                {
+                    CheckBoxCheked = false,
+                    Subject = i + "Check this mail!",
+                    CreatedOn = DateTime.Now,
+                    Category = "Work"
+                };
+
+                notesData.Add(note);
+            }
+
+            if (search != null && search != String.Empty)
+            {
+                notesData = notesData.FindAll(n => n.Subject.Contains(search));
+            }
+
+            return Json(notesData.ToDataSourceResult(request));
+        }
+    }
 }
---
+++ TelerikMvcWebMail/Models/NoteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelerikMvcWebMail.Models
+{
+    public class NoteViewModel
+    {
+        public int Id { get; set; }
+
+        public bool CheckBoxCheked { get; set; }
+
+        public string Subject { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string Category { get; set; }
+    }
+}
