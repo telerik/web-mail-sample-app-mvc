cd062b6b536d53f12a4a86a924646b53384b8881
ivan.danchev <ifujin@gmail.com>
2016-12-14 09:28:30 +0200

    Add SignIn view and change Menu in TreeView partial to ActionLinks

---
+++ TelerikMvcWebMail/Controllers/SignInController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelerikMvcWebMail.Controllers
+{
+    public class SignInController : Controller
+    {
+        //
+        // GET: /SignIn/
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+	}
+}
