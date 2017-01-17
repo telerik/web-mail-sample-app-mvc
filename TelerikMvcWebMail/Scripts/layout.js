c51a6743bf0e34eb832896ff9b8284f225837afc
ivan.danchev <ifujin@gmail.com>
2017-01-17 10:07:22 +0200

    fix: fixed User Profile Tooltip close behavior to resemble that of a Menu and close on click outside of it

--- TelerikMvcWebMail/Scripts/layout.js
+++ TelerikMvcWebMail/Scripts/layout.js
@@ -2,16 +2,3 @@
     location.href = location.protocol + '//' + location.host + '/SignIn/Index';
     return false;
 }
-
-function closeToolTip(e) {
-    var tooltip = $("#mainMenu > li:last").data("kendoTooltip");
-    tooltip.hide();
-}
-
-function tooltipShow(e) {
-    document.body.addEventListener("mouseup", closeToolTip);
-}
-
-function tooltipHide(e) {
-    document.body.removeEventListener("mouseup", closeToolTip);
-}
