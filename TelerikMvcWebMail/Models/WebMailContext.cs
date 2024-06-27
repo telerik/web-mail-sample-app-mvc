5debca1fa9ef308f70f9850e619f225dfebeb5a9
alestoya <alestoya@progress.com>
2024-06-27 10:02:54 +0300

    chore: migrate webmail app to sqllite telerik/kendo#19394

---
+++ TelerikMvcWebMail/Models/WebMailContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.SQLite;
+
+namespace TelerikMvcWebMail.Models
+{
+
+    public partial class WebMailEntities : DbContext
+    {
+        public WebMailEntities()
+            : base(new SQLiteConnection()
+            {
+                ConnectionString = new SQLiteConnectionStringBuilder()
+                {
+                    DataSource = $"{AppDomain.CurrentDomain.GetData("DataDirectory").ToString()}\\demos.db",
+                    ForeignKeys = true
+                }.ConnectionString
+            },
+            true)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+
+        public virtual DbSet<Event> Events { get; set; }
+        public virtual DbSet<Mail> Mails { get; set; }
+        public virtual DbSet<Person> People { get; set; }
+        public virtual DbSet<Task> Tasks { get; set; }
+    }
+}
